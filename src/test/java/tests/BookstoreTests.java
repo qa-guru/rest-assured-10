c3f01421f1da7fdc170bb9fd9fc9114dfe8f795e
svasenkov <aanher@gmail.com>
2022-03-08 20:50:50 +0300

    added bookstore test with log

---
+++ src/test/java/tests/BookstoreTests.java
@@ -0,0 +1,71 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class BookstoreTests {
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://demoqa.com";
+    }
+
+    @Test
+    void getBooksTest() {
+        get("/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void getBooksWithAllLogsTest() {
+        given()
+                .log().all()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+                .log().all()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void getBooksWithSomeLogsTest() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .body("books", hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void generateTokenTest() {
+        String data = "{ \"userName\": \"alex\", " +
+                "\"password\": \"asdsad#frew_DFS2\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/Account/v1/GenerateToken")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("status", is("Success"))
+                .body("result", is("User authorized successfully."))
+                .body("token.size()", greaterThan(10));
+    }
+
+
+}
